4783abfcaac68458c25d8dec6b7d8c9211f35d6a
yuxin1106 <yuxin@usc.edu>
2018-08-31 18:34:15 -0700

    Add files via upload

---
+++ LSFR.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+/*
+ File: LSFR
+ Author: Xin Yu
+*/
+module LSFR(out, clk, rst);
+
+output reg [3:0] out;
+input clk, rst;
+
+wire feedback;
+
+assign feedback = ~(out[3]^out[2]);
+
+always @(posedge clk, posedge rst)
+begin
+   if (rst)
+      out = 4'b0;
+   else
+      out = {out[2:0], feedback};
+end
+endmodule
---
+++ LSFR_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+
+module LSFR_tb();
+reg clk_tb;
+reg rst_tb;
+wire [3:0] out_tb;
+
+initial
+begin
+   clk_tb = 0;
+   rst_tb = 1;
+   #15;
+   
+   rst_tb = 0;
+   #200;
+end
+
+always
+begin
+   #5;
+   clk_tb = ~ clk_tb;
+end
+
+LSFR DUT(out_tb, clk_tb, rst_tb);
+
+endmodule
